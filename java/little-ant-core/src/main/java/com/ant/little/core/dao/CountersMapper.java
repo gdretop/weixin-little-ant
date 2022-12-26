a9b501f6810a603de08cdf6ba67ef9fa5ad8bc88
yuwanglin <2292957362@qq.com>
2022-12-26 10:29:30 +0800

    重构

--- java/little-ant-core/src/main/java/com/ant/little/core/dao/CountersMapper.java
+++ java/little-ant-core/src/main/java/com/ant/little/core/dao/CountersMapper.java
@@ -7,9 +7,9 @@ import org.apache.ibatis.annotations.Param;
 @Mapper
 public interface CountersMapper {
 
-  Counter getCounter(@Param("id") Integer id);
+    Counter getCounter(@Param("id") Integer id);
 
-  void upsertCount(Counter counter);
+    void upsertCount(Counter counter);
 
-  void clearCount(@Param("id") Integer id);
+    void clearCount(@Param("id") Integer id);
 }
--- java/little-ant-web/src/main/java/com/ant/little/web/Application.java
+++ java/little-ant-web/src/main/java/com/ant/little/web/Application.java
@@ -3,9 +3,8 @@ package com.ant.little.web;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication(scanBasePackages = {"com.ant.little"})
-@MapperScan(basePackages = {"com.ant.little.core.dao"})
+@MapperScan(basePackages = {"com.ant.little.core.dao","com.ant.little.model"})
 public class Application {
 
     public static void main(String[] args) {
