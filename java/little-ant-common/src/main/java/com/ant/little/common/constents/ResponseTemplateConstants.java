2bcf021a7d6c92a5a3d915cbb6166f6656aaa9a9
yuwanglin <2292957362@qq.com>
2023-01-02 01:37:27 +0800

    修改文案

--- java/little-ant-common/src/main/java/com/ant/little/common/constents/ResponseTemplateConstants.java
+++ java/little-ant-common/src/main/java/com/ant/little/common/constents/ResponseTemplateConstants.java
@@ -8,5 +8,19 @@ package com.ant.little.common.constents;
  **/
 public class ResponseTemplateConstants {
     public static String SERVER_ERROR = "服务器开小差了,等待管理员处理^_^【目前只支持 最佳路线 最短路径两种 指令,确认坐标用英文逗号隔开,没有多余换行】";
-    public static String CAN_NOT_FIND_ANSWER_SERVICE = "找不到相关指令@.@!,等待管理员查看后处理哈^_^\n(或者查看相关文档确认一下输入格式)";
+//    public static String CAN_NOT_FIND_ANSWER_SERVICE = "找不到相关指令@.@!,等待管理员查看后处理哈^_^\n(或者查看相关文档确认一下输入格式)";
+    public static String CAN_NOT_FIND_ANSWER_SERVICE = "欢迎关注，点击下方菜单生存之路->指令教程可查看详细指令。或者点击右上角查看文章列表找到指令攻略。\n" +
+        "\n" +
+        "最短路径指令：至少输入3行,2个点坐标,换行分割点,空格分割坐标。最后一行可不填，表示你的剩余步数，如下所示\n" +
+        "最短路径\n" +
+        "101 101\n" +
+        "202 202\n" +
+        "100\n" +
+        "\n" +
+        "最佳路线指令：输入5行,4个点坐标,换行分割点,空格分割坐标。如下所示\n" +
+        "最佳路线\n" +
+        "20 200\n" +
+        "101 101\n" +
+        "202 202\n" +
+        "303 303";
 }
