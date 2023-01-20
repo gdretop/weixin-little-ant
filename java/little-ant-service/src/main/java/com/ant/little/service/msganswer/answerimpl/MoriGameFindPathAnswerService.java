6614492eede6f55aab2039755d40ad3e8e2fc81b
yuwanglin <2292957362@qq.com>
2023-01-20 23:41:19 +0800

    修复5秒超时问题

--- java/little-ant-service/src/main/java/com/ant/little/service/msganswer/answerimpl/MoriGameFindPathAnswerService.java
+++ java/little-ant-service/src/main/java/com/ant/little/service/msganswer/answerimpl/MoriGameFindPathAnswerService.java
@@ -129,6 +129,10 @@ public class MoriGameFindPathAnswerService implements MsgAnswerBaseService {
             WxSubMsgResponseDTO wxSubMsgResponseDTO = wxSubMsgDTO.toResponse();
             wxSubMsgResponseDTO.setMsgType(WxMsgTypeEnum.TEXT.getName());
             String result = runTimeResponse.getResultString();
+            if(result.length() > 1180) {
+                result = result.substring(0,1180);
+                result = result+"\n【路线太长,不展示了,先走一会吧】";
+            }
             result = result + "\n\n公众号:旺仔小蚂蚁";
             wxSubMsgResponseDTO.setContent(result);
             localCache.put(content, result);
